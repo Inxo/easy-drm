6886d9ac29eeb92dbd20f661c99a0c8f89a59731
Dmitrii Larionov <inxo@inxo.ru>
2024-02-11 19:33:15 +0300

    WP

--- src/main.go
+++ src/main.go
@@ -3,70 +3,56 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
-	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 )
 
 var aesKey = []byte("your_secret_aes_key_32_charslong")
 
-func encrypt(data []byte, key []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-
-	ciphertext := gcm.Seal(nil, nonce, data, nil)
-	ciphertext = append(nonce, ciphertext...)
-	return ciphertext, nil
-}
-
-var wd = ""
-
 func main() {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Cannot get working directory")
-	}
-	fmt.Println("wd: " + wd)
 	http.HandleFunc("/video", func(w http.ResponseWriter, r *http.Request) {
-
-		// Чтение mp4-файла
-		file, err := os.Open(wd + "/data/input.mp4")
+		// Открытие файла
+		file, err := os.Open("input.mp4")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer file.Close()
 
-		videoData, err := ioutil.ReadAll(file)
+		// Создание блочного шифра
+		block, err := aes.NewCipher(aesKey)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		encryptedData, err := encrypt(videoData, aesKey)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		// Создание CTR режима шифрования
+		stream := cipher.NewCTR(block, aesKey[:block.BlockSize()])
 
+		// Отправка заголовков
 		w.Header().Set("Content-Type", "application/octet-stream")
-		w.Write(encryptedData)
+		w.Header().Set("Content-Disposition", "attachment; filename=\"video.mp4\"")
+
+		// Создание шифрованного потока записи
+		writer := &cipher.StreamWriter{S: stream, W: w}
+
+		// Чтение данных файла по блокам, их шифрование и запись в ответ
+		buffer := make([]byte, 8192) // Буфер размером 8KB
+		for {
+			n, err := file.Read(buffer)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if _, err := writer.Write(buffer[:n]); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
 	})
 
 	// Middleware для обработки CORS
@@ -82,5 +68,8 @@ func main() {
 		})
 	}
 
-	http.ListenAndServe(":8080", corsMiddleware(http.DefaultServeMux))
+	err := http.ListenAndServe(":8080", corsMiddleware(http.DefaultServeMux))
+	if err != nil {
+		return
+	}
 }
